166fde1d5affce5ca73fb798d541931689c331d1
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-02-15 23:39:19 -0400

    Segundo commit

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import multiplicador from "./multiplicador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const tercero = document.querySelector("#tercer-numero");
+const cuarto = document.querySelector("#cuarto-numero");
+const multiform = document.querySelector("#multi-form");
+const multidiv = document.querySelector("#resultado-mult-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +19,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tercernumero = Number.parseInt(tercero.value);
+  const cuartonumero = Number.parseInt(cuarto.value);
+
+  multidiv.innerHTML = "<p>" + multiplicar(tercernumero, cuartonumero) + "</p>";
 });
