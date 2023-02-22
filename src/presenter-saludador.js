48ef7a137462ec8f117d0c92ab3d842009026317
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-02-21 20:19:43 -0400

    Saludo con edad

--- src/presenter-saludador.js
+++ src/presenter-saludador.js
@@ -2,12 +2,16 @@ const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#saludar-div");
 const genero = document.querySelector("#genero")
+const edad = document.querySelector("#edad");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  if(genero.value == "Masculino")
-      div.innerHTML = "Hola Sr. " + nombre.value;
-  else if(genero.value == "Femenino")
-      div.innerHTML = "Hola Sra. " + nombre.value;
+  if(edad.value > 30)
+      if(genero.value == "Masculino")
+       div.innerHTML = "Hola Sr. " + nombre.value;
+      else if(genero.value == "Femenino")
+        div.innerHTML = "Hola Sra. " + nombre.value;
+  if(edad.value <= 30)
+      div.innerHTML = "Hola " + nombre.value;
 });
