ebe2bdf47489f69ec4d9b075862aaf062a156587
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-02-23 00:02:25 -0400

    Ingresar Cantidad y mostrar

--- src/presenter.js
+++ src/presenter.js
@@ -1,31 +1,10 @@
-import sumar from "./sumador";
-import multiplicador from "./multiplicador";
-import multiplicar from "./multiplicador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
-const tercero = document.querySelector("#tercer-numero");
-const cuarto = document.querySelector("#cuarto-numero");
-const multiform = document.querySelector("#multi-form");
-const multidiv = document.querySelector("#resultado-mult-div");
+const cantItems = document.querySelector("#cantidad");
+const form = document.querySelector("#Mostrar-form");
+const div = document.querySelector("#Mostrar");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-multiform.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const tercernumero = Number.parseInt(tercero.value);
-  const cuartonumero = Number.parseInt(cuarto.value);
-
-  multidiv.innerHTML = "<p>" + multiplicar(tercernumero, cuartonumero) + "</p>";
+  div.innerHTML = "Cantidad de items: " + cantItems.value ;
 });
